388129188ce94f9a5b6f4a600c1e91cf3bb11283
NASIR <NASIR@DESKTOP-E1T3NF4>
2023-08-01 21:34:16 +0530

    final commit

---
+++ sting/src/sting/cvbnm.java
@@ -0,0 +1,28 @@
+package sting;
+
+public class cvbnm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello";
+		String z;
+		s=s.replace("l","z");
+	 System.out.println(s);
+	 String[] straaray= {"hey", "hello", "how are you"};
+	 int[] n= {1,2,3,4,5,6,7,};
+	 
+	 for(String str:straaray)
+	 {
+		 System.out.println(str);
+	 }
+	 
+	 for(int str:n)
+	 {
+		 System.out.println(str);
+	 }
+	 
+		
+
+	}
+
+}
